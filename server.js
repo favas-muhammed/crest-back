d2d9b6c8aca462140d87606619342bf8c8c3f96f
Favas Muhammed <favassakkeer13@gmail.com>
2025-08-21 01:02:43 +0200

    ..

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// CORS configuration
+// Updated CORS configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://crest-front.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
