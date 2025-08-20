0efd8b8e52aa4c623ac15fd860363fb559c7293c
Favas Muhammed <favassakkeer13@gmail.com>
2025-08-20 19:05:50 +0200

    Update CORS configuration

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(
     origin: "https://crest-front.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
   })
 );
 
