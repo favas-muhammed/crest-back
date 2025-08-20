d460f1906ee5ae9539e921c7d9459aed058795d1
Favas Muhammed <favassakkeer13@gmail.com>
2025-08-21 01:21:52 +0200

    ...

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: 'https://crest-front.vercel.app',
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 // Explicitly handle preflight requests
